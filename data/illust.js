cab3ec21b8f6e828ace3737825ea6ce626b34be6
Organ <80056658+OrganThousand@users.noreply.github.com>
2025-12-14 23:46:04 +0900

    data.jsを削除し、作品媒体で分離。更新通知をトップに追加

---
+++ data/illust.js
@@ -0,0 +1,51 @@
+// data/illust.js
+window.ILLUSTS = [
+  {
+    id: "illust_001",
+    title: "神話世界の旅人",
+    year: "2025",
+    date: "2025-12-14",
+    caption: "神話世界を旅する人物。",
+    image: "images/2loras_test__00037_.png",
+    tags: ["fantasy", "myth", "original"],
+    worldLabel: "神話世界",
+    links: {
+      manga: "../comic/work1/index.html",
+      game: "../game/game1.html"
+    }
+  },
+  {
+    id: "illust_002",
+    title: "森と機械",
+    year: "2025",
+    date: "2025-12-01",
+    caption: "自然と機械が溶け合う境界。",
+    image: "images/illust_02.jpg",
+    tags: ["fantasy", "original"],
+    worldLabel: "自然×機械",
+    links: {
+      manga: "../comic/work1/index.html",
+      game: "../game/game1.html"
+    }
+  },
+  {
+    id: "illust_003",
+    title: "風の神",
+    year: "2024",
+    date: "2024-11-20",
+    caption: "風そのものを神格化した存在。",
+    image: "images/illust_03.jpg",
+    tags: ["myth", "original"],
+    worldLabel: "神話世界",
+    links: {
+      manga: "../comic/work2/index.html"
+    }
+  }
+];
+
+window.TAG_LABELS = {
+  all: "すべて",
+  fantasy: "ファンタジー",
+  myth: "神話",
+  original: "オリジナル"
+};
---
+++ data/updates.js
@@ -0,0 +1,25 @@
+// data/updates.js
+window.UPDATES = [
+    {
+      type: "illust",
+      title: "神話世界の旅人",
+      date: "2025-12-14",
+      image: "illust/images/2loras_test__00037_.png",
+      link: "illust/view.html?id=illust_001"
+    },
+    {
+      type: "comic",
+      title: "第3話 公開",
+      date: "2025-12-10",
+      image: "comic/images/cover_work1.jpg",
+      link: "comic/work1/003.html"
+    },
+    {
+      type: "game",
+      title: "体験版 ver1.1",
+      date: "2025-12-01",
+      image: "game/images/game1_thumb.jpg",
+      link: "game/index.html"
+    }
+  ];
+  
